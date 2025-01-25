bb8ff986b0b6e2a6a7258f49366cb8b5fdb53944
Ayaan <ghaurifx@gmail.com>
2025-01-25 18:15:16 +0500

    Welcomes user and shows balance , shows number of accounts on home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import HeaderBox from "@/components/ui/HeaderBox";
+import React from "react";
 
 const HOME = () => {
+  const loggedIn = { firstName: "Farooq" };
   return (
-    <div>HOME</div>
-  )
-}
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transaction efficiently"
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+      </div>
+    </section>
+  );
+};
 
-export default HOME
+export default HOME;
